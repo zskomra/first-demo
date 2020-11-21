8fffb0df330b16ed6a0540660eb252a583665cc7
z.skomra <z.skomraa@gmail.com>
2020-11-21 10:26:54 +0100

    structure :: initial project structure

---
+++ src/main/java/projects/portfoliodemo/PortfolioDemoApplication.java
@@ -0,0 +1,13 @@
+package projects.portfoliodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioDemoApplication.class, args);
+	}
+
+}
